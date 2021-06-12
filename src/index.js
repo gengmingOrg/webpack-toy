8b8ee9a1ab1c5f131b5446f0445e595f6d34625c
gengming <gengming@xuetangx.com>
2021-06-12 15:54:30 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
- import _ from 'lodash';
-
+import _ from 'lodash';
+import printMe from './print.js'
 function component() {
     let element = document.createElement('div');
+    var btn = document.createElement('button');
+    btn.innerHTML = '点击这里，然后查看 console！';
+    btn.onclick = printMe;
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.appendChild(btn);
     return element;
 }
 document.body.appendChild(component());
