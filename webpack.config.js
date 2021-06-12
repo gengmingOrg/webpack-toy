cb7173954e035ef3d9d6d5ef9c1a44f0bcf06732
gengming <gengming@xuetangx.com>
2021-06-12 16:51:49 +0800

    管理输出加 html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: {
     app: './src/index.js',
@@ -7,6 +9,12 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出1'
+    })
+  ],
 };
