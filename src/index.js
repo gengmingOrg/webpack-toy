3b1ab92b226095f258101c9482d2041322572ae2
gengming <gengming@xuetangx.com>
2021-06-13 12:46:04 +0800

    添加代码分离

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import { cube } from './math.js';
-import _ from 'lodash';
-function component() {
-    var element = document.createElement('div');
-    var element = document.createElement('pre');
-    
-    // lodash 是由当前 script 脚本 import 进来的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.innerHTML = [
-        'Hello webpack!',
-        '5 cubed is equal to have ' + cube(5)
-    ].join('\n\n');
 
-    return element;
+function getComponent() {
+    return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
+        var element = document.createElement('div');
+        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+        return element;
+    }).catch(error => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then(component => {
+    document.body.appendChild(component);
+})
--- webpack.common.js
+++ webpack.common.js
@@ -6,11 +6,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     app: './src/index.js',
-    another: './src/another-module.js'
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    chunkFilename: '[name].bundle.js', //它决定 non-entry chunk(非入口 chunk) 的名称
     clean: true,
   },
   plugins: [
